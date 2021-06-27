af6e43237d9cafa4fc46302dadb42d37617c9a79
gmamillon <72396118+gmamillon@users.noreply.github.com>
2021-06-27 13:00:20 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
-mongoose.connect('mongodb+srv://gabriel:joyoboyo@cluster0.ex3lj.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://mainAdmin:joyoboyo@cluster0.ex3lj.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
