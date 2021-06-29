3553826753225024840860e85221b053e890be36
gmamillon <gabrielmamillon@gmail.com>
2021-06-29 21:31:12 +0200

    correction

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -45,58 +44,7 @@ exports.login = (req, res, next) => {
 };
 
 exports.deleteUser = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
+    user.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Utilisateur supprimé !'}))
     .catch(error => res.status(400).json({ error }));
-=======
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-
-// Hachage du MDP et enregistrement du nouvel utilisateur dans la BDD.
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message : "Utilisateur créé."}))
-        .catch(error => res.status(400).json({ error: "Adresse e-mail déjà attribuée" }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};
-
-// Vérification des identifiants de l'utilisateur lors de la connexion.
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-    .then(user => {
-        if (!user) {
-            return res.status(401).json({ error: 'Utilisateur non trouvé.'});
-        }
-        bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            if (!valid) {
-                return res.status(401).json({ error: "Mot de passe incorrect."});
-            }
-            res.status(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                    { userId: user._id},
-                    'LA_VILLE_DE_SOTCHI_ETE_PEUPLE_DE_368011_HABITANTS_EN_2013',
-                    { expiresIn: '24h' }
-                )
-            });
-        })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};
-
-exports.deleteUser = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Utilisateur supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
->>>>>>> cb9d2415423365b5ab1e962b26d85f6693ccd5a3
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 
@@ -9,16 +8,4 @@ router.post('/signup', validator, authCtrl.signup);
 router.post('/login', validator, authCtrl.login);
 router.post('/delete', authCtrl.deleteUser);
 
-=======
-const express = require('express');
-const router = express.Router();
-
-const authCtrl = require('../controllers/user');
-const validator = require('../middleware/validatorEmail');
-
-router.post('/signup', validator, authCtrl.signup);
-router.post('/login', validator, authCtrl.login);
-router.post('/delete', authCtrl.deleteUser);
-
->>>>>>> cb9d2415423365b5ab1e962b26d85f6693ccd5a3
 module.exports = router;
